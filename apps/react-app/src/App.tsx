a9ffb1781c418f35c97d8dbe4c3ab09bc5bb3156
ronakmaheshwari077 <ronakmaheshwari077@google.com>
2025-04-18 15:25:20 +0530

    Added Prisma Next React Http WS

---
+++ apps/react-app/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@repo/ui/button"
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <Button children="Submit ME" appName={"react-app"}  />
+    </div>
+  )
+}
+
+export default App
