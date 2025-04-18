d51362ae17f2b820012c20727c3ead7d2d1abf10
ronakmaheshwari077 <ronakmaheshwari077@google.com>
2025-04-18 21:23:15 +0530

    Added Storybook Fixed Issues

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello WOrld")
+import express,{Request,Response} from "express"
+import {Backend_URL} from "@repo/common/config"
+
+const app = express()
+const port = process.env.PORT || 3001
+const url = Backend_URL
+console.log(url);
+
+app.get("/",async(req:Request,res:Response)=>{
+    res.json({
+        message:"You are Welcome"
+    })
+})
+
+app.listen(port,()=>{
+    console.log(`Server running on Port http://localhost:${port}`)
+})
--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,3 +1,4 @@
 export const value = "Ronak MAheshwari";
 
 export const Backend_URL = "https://projects.100xdevs.com/tracks/monorepo/monorepo-18"
+
